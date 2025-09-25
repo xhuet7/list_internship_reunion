832ae2268f94a9a42fec9163523175464a0af4ad
mmarya045 <marya.madi@epitech.eu>
2025-09-25 23:42:14 +0400

    [ADD] center map

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from 'react'
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <footer>
+        <p className="text-xs text-white text-center">&copy; 2025. JEB. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
